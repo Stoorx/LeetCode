26052a422ade2d7bb144c97b561b0caffc3d89ee
Stoorx <me@stoorx.one>
2024-06-30 19:22:23 +0300

    Add better solution of problem [23. Merge k Sorted Lists]

--- src/main/java/one/stoorx/leetcode/algorithms/MergeKSortedLists.java
+++ src/main/java/one/stoorx/leetcode/algorithms/MergeKSortedLists.java
@@ -3,23 +3,29 @@ package one.stoorx.leetcode.algorithms;
 
 import one.stoorx.leetcode.algorithms.util.ListNode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
 // Task: 23. Merge k Sorted Lists
 // Link: https://leetcode.com/problems/merge-k-sorted-lists
 // Level: Hard
 public class MergeKSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
-        int[] counting = new int[20001];
-        for (ListNode list : lists) {
-            for (ListNode currentNode = list; currentNode != null; currentNode = currentNode.next) {
-                ++counting[currentNode.val + 10000];
-            }
+        if (lists == null)
+            return null;
+        PriorityQueue<ListNode> listNodes = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+        listNodes.addAll(Arrays.stream(lists).filter(Objects::nonNull).toList());
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (listNodes.size() > 0) {
+            ListNode minimumNode = listNodes.remove();
+            tail.next = minimumNode;
+            tail = minimumNode;
+            if (minimumNode.next != null)
+                listNodes.add(minimumNode.next);
         }
-        ListNode currentListNode = null;
-        for (int i = counting.length - 1; i >= 0; --i) {
-            for (int j = 0; j < counting[i]; ++j) {
-                currentListNode = new ListNode(i - 10000, currentListNode);
-            }
-        }
-        return currentListNode;
+        return head.next;
     }
 }
